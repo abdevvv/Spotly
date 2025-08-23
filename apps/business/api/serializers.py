452ff8b1da443e0d01fd228bbcac2519516080db
7amota <bm993508@gmail.com>
2025-08-24 01:47:13 +0300

    refactor: add viewset and handle the list and retrieve logic

--- apps/business/api/serializers.py
+++ apps/business/api/serializers.py
@@ -3,11 +3,23 @@ from rest_framework import serializers
 
 from apps.business.models import Business,Favorite,Category
 
-class BusinessSerializer(serializers.ModelSerializer):
+class BusinessListSerializer(serializers.ModelSerializer):
     location = serializers.SerializerMethodField()
+    distance = serializers.CharField(required=False)
     class Meta:
         model = Business
-        fields = ['name','location']
+        fields = ['id','name','location','distance']
+    
+    def get_location(self,obj):
+        return [obj.location.x, obj.location.y] if obj.location else None
+
+    
+class BusinessDetailSerializer(serializers.ModelSerializer):
+    location = serializers.SerializerMethodField()
+    distance = serializers.CharField(required=False)
+    class Meta:
+        model = Business
+        fields = "__all__"
     
     def get_location(self,obj):
         return [obj.location.x, obj.location.y] if obj.location else None
--- apps/business/api/views/business.py
+++ apps/business/api/views/business.py
@@ -1,17 +1,24 @@
 from django.contrib.gis.geos import Point
 from django.contrib.gis.db.models.functions import Distance
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 from apps.business.api.filters import BusinessFilter
 from apps.business.models import Business
-from apps.business.api.serializers import BusinessSerializer
+from apps.business.api.serializers import BusinessListSerializer, BusinessDetailSerializer
 
-class BusinessList(generics.ListAPIView):
+class BusinessViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()
-    serializer_class = BusinessSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = BusinessFilter
+    def get_serializer_class(self):
+        action = self.action
+        if action == 'list':
+            return BusinessListSerializer
+        if action == 'retrieve':
+            return BusinessDetailSerializer
+    
+ 
