ae4e0f0b7b7dada0872e9b873d57bdfd616f2aeb
7amota <bm993508@gmail.com>
2025-08-24 01:47:13 +0300

    refactor: add viewset and handle the list and retrieve logic

--- apps/business/api/urls.py
+++ apps/business/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 
 from apps.business.api.views import business
-urlpatterns = [
-    path('business/',business.BusinessList.as_view())
-]
+router = DefaultRouter()
+router.register(r'businesses', business.BusinessViewSet, basename='business')
+
+urlpatterns = router.urls
